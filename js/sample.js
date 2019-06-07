2b8c3f3a331ebea94fa763b921d5b8a3560f98dd
gigamagro <51286221+gigamagro@users.noreply.github.com>
2019-06-07 19:02:04 +0900

    うごくところまで

--- js/sample.js
+++ js/sample.js
@@ -1,39 +1,57 @@
 
-var el = $('.menu-btn');
 
 $('.menu-icon').click(function() {
     // チェックされたとき
     if ($('.menu-icon__cheeckbox').prop('checked')) {
         $('.btn').css('animation', 'none');
+        $('.btn:hover').css(
+            {
+                'background': 'steelBlue',
+                'border': 'solid 3px #38678f'
+            }
+        );
 
-        const a1 = anime.timeline(
-        {
+        const a1 = anime.timeline({
             targets: '.btn',
-            duration: 1000,
-            translateY: -200,
+            duration: 100,
             easing: 'linear',
+        }).add({
+            translateY: '-200px',
             borderRadius: ['50%', '0%'],
         }).add({
-            targets: '.btn',
-            duration: 1000,
             width: '75%',
-            height: '75%',
+            bottom: '10px',
             easing: 'linear',
-        });
+        }).add({
+            targets: '.chat',
+            duration: 300,
+            opacity: 1
+        }, '+=200');
+
+
+    // check off の時(初期状態へ戻す)
     } else {
         console.log('checked');
+        $('.btn').css('animation', 'pulsebtn 2s infinite');
+        $('.btn:hover').css(
+            {
+                'background': 'steelBlue',
+                'border': 'solid 3px #294d6b'
+            }
+        )
 
-        const a2 = anime.timeline(
-        {
+        const a2 = anime.timeline({
             targets: '.btn',
+            duration: 100,
+            easing: 'linear',
+        }).add({
+            targets: '.chat',
+            opacity: 0
+        }).add({
             width: '70px',
             height: '70px',
-            easing: 'linear',
-            
         }).add({
-            targets: '.btn',
-            duration: 500,
-            translateY: 0,
+            translateY: '0px',
             easing: 'linear',
             borderRadius: ['0%', '50%']
         });
