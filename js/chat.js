2a49dc90cdd58d34bed7c19db40b20d8d2c4d363
gigamagro <51286221+gigamagro@users.noreply.github.com>
2019-06-12 18:30:44 +0900

    prettify

--- js/chat.js
+++ js/chat.js
@@ -1,93 +1,96 @@
-
 // 自分が入力した値
-var message = '<div class="bms_message bms_right"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text"></div></div></div></div><div class="bms_clear"></div>';
+var message =
+  '<div class="bms_message bms_right"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text"></div></div></div></div><div class="bms_clear"></div>';
 
 // サーバからの戻り値
-var ksas_message = '<div class="bms_message bms_left"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text"></div></div></div></div><div class="bms_clear"></div>';
+var ksas_message =
+  '<div class="bms_message bms_left"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text"></div></div></div></div><div class="bms_clear"></div>';
 
 // loading中のメッセージ
-var loading_message = '<div class="bms_message bms_left loading_message"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text"><p class="loading"><span>解</span><span>析</span><span>中</span></p></div></div></div></div>';
+var loading_message =
+  '<div class="bms_message bms_left loading_message"><div class="bms_message_box"><div class="bms_message_content"><div class="bms_message_text"><p class="loading"><span>解</span><span>析</span><span>中</span></p></div></div></div></div>';
 
 // スムーススクロール
 var moveTo = new MoveTo({
-    tolerance: 0,
-    duration: 1000,
-    easing: 'easeOutQuart',
-    container: $('#bms_messages')[0],
+  tolerance: 0,
+  duration: 1000,
+  easing: "easeOutQuart",
+  container: $("#bms_messages")[0]
 });
 
+$("#bms_send_btn").click(function() {
+  var url = "http://localhost:3000/getWords";
 
-$("#bms_send_btn").click( function(){
-    var url = "http://localhost:3000/getWords";
+  var text = $("#bms_send_message").val();
+  $("#bms_send_message").val("");
 
-    var text = $("#bms_send_message").val();
-    $("#bms_send_message").val("");
-    
-    // 文字がなかったら何もしない
-    if (text === "") return ;
+  // 文字がなかったら何もしない
+  if (text === "") return;
 
-    var JSONdata = {
-        text: text   
-    };
+  var JSONdata = {
+    text: text
+  };
 
-    // 自分が入力した値を追加する
-    var m = $(message);
-    m.appendTo('#bms_messages');
-    //   alert(JSON.stringify(JSONdata));
-    m.find(".bms_message_text").text(text);    
+  // 自分が入力した値を追加する
+  var m = $(message);
+  m.appendTo("#bms_messages");
+  //   alert(JSON.stringify(JSONdata));
+  m.find(".bms_message_text").text(text);
 
-    moveTo.move($(".bms_message").last()[0]);
+  moveTo.move($(".bms_message").last()[0]);
 
-    // loading画面
-    var loading = $(loading_message);
-    loading.appendTo('#bms_messages');
-    // ボタンを無効にする
-    $('#bms_send_btn').prop("disabled", true);
+  // loading画面
+  var loading = $(loading_message);
+  loading.appendTo("#bms_messages");
+  // ボタンを無効にする
+  $("#bms_send_btn").prop("disabled", true);
 
-    // サーバに送信する
-    $.ajax({
-        type : 'post',
-        url : url,
-        data : JSON.stringify(JSONdata),
-        contentType: 'application/json',
-        dataType : 'JSON',
-        scriptCharset: 'utf-8',
-        success : function(res) {
+  // サーバに送信する
+  $.ajax({
+    type: "post",
+    url: url,
+    data: JSON.stringify(JSONdata),
+    contentType: "application/json",
+    dataType: "JSON",
+    scriptCharset: "utf-8",
+    success: function(res) {
+      // loading画面の削除
+      $(".loading_message").remove();
 
-            // loading画面の削除
-            $('.loading_message').remove();
+      // レスポンスメッセージを追加
+      var ksas_m = $(ksas_message);
+      ksas_m.appendTo("#bms_messages");
+      ksas_m.find(".bms_message_text").html(res.message);
 
-            // レスポンスメッセージを追加
-            var ksas_m = $(ksas_message);
-            ksas_m.appendTo('#bms_messages');
-            ksas_m.find(".bms_message_text").html(res.message);
-            
-            moveTo.move($(".bms_message").last()[0]);
+      moveTo.move($(".bms_message").last()[0]);
 
-            // 送信ボタンを有効にする
-            $('#bms_send_btn').prop("disabled", false);
-        },
-        error : function(res) {
-            // loading画面の削除
-            $('.loading_message').remove();
+      // 送信ボタンを有効にする
+      $("#bms_send_btn").prop("disabled", false);
+    },
+    error: function(res) {
+      // loading画面の削除
+      $(".loading_message").remove();
 
-            // エラーメッセージを追加
-            var ksas_m = $(ksas_message);
-            ksas_m.appendTo('#bms_messages');
-            ksas_m.find(".bms_message_text").html('すみません、ご質問の答えが分かりません。</br>他の言い方でもう一度質問するか、</br>サービスデスクまでお問い合わせください。</br>KSASサービスデスク:0120-527-800');
- 
-            moveTo.move($(".bms_message").last()[0]);
+      // エラーメッセージを追加
+      var ksas_m = $(ksas_message);
+      ksas_m.appendTo("#bms_messages");
+      ksas_m
+        .find(".bms_message_text")
+        .html(
+          "すみません、ご質問の答えが分かりません。</br>他の言い方でもう一度質問するか、</br>サービスデスクまでお問い合わせください。</br>KSASサービスデスク:0120-527-800"
+        );
 
-            // 送信ボタンを有効にする
-            $('#bms_send_btn').prop("disabled", false);
-        }
-    });
-})
+      moveTo.move($(".bms_message").last()[0]);
+
+      // 送信ボタンを有効にする
+      $("#bms_send_btn").prop("disabled", false);
+    }
+  });
+});
 
 // enterキーを押す
-function enter(){
-   if( window.event.keyCode == 13 ){
-      $("#bms_send_btn").click();
-   }
+function enter() {
+  if (window.event.keyCode == 13) {
+    $("#bms_send_btn").click();
+  }
 }
-
--- js/sample.js
+++ js/sample.js
@@ -1,62 +1,65 @@
+$(".menu-icon").click(function() {
+  // チェックされたとき
+  if ($(".menu-icon__cheeckbox").prop("checked")) {
+    $(".btn").css("animation", "none");
+    $(".btn:hover").css({
+      background: "#00a8a9",
+      border: "solid 3px #007576"
+    });
 
-
-$('.menu-icon').click(function() {
-    // チェックされたとき
-    if ($('.menu-icon__cheeckbox').prop('checked')) {
-        $('.btn').css('animation', 'none');
-        $('.btn:hover').css(
-            {
-                'background': '#00a8a9',
-                'border': 'solid 3px #007576'
-            }
-        );
-
-        const a1 = anime.timeline({
-            targets: '.btn',
-            duration: 100,
-            easing: 'linear',
-        }).add({
-            translateY: '-200px',
-            borderRadius: ['50%', '0%'],
-        }).add({
-            width: '40%',
-            height: '80%',
-            bottom: '-200px',
-            easing: 'linear',
-        }).add({
-            targets: '.chat',
-            duration: 300,
-            opacity: 1
-        }, '+=200');
-
+    const a1 = anime
+      .timeline({
+        targets: ".btn",
+        duration: 100,
+        easing: "linear"
+      })
+      .add({
+        translateY: "-200px",
+        borderRadius: ["50%", "0%"]
+      })
+      .add({
+        width: "40%",
+        height: "80%",
+        bottom: "-200px",
+        easing: "linear"
+      })
+      .add(
+        {
+          targets: ".chat",
+          duration: 300,
+          opacity: 1
+        },
+        "+=200"
+      );
 
     // check off の時(初期状態へ戻す)
-    } else {
-        console.log('checked');
-        $('.btn').css('animation', 'pulsebtn 2s infinite');
-        $('.btn:hover').css(
-            {
-                'background': '#00a8a9',
-                'border': 'solid 3px #007576'
-            }
-        )
+  } else {
+    console.log("checked");
+    $(".btn").css("animation", "pulsebtn 2s infinite");
+    $(".btn:hover").css({
+      background: "#00a8a9",
+      border: "solid 3px #007576"
+    });
 
-        const a2 = anime.timeline({
-            targets: '.btn',
-            duration: 100,
-            easing: 'linear',
-        }).add({
-            targets: '.chat',
-            opacity: 0
-        }).add({
-            width: '70px',
-            height: '70px',
-            bottom: '20px'
-        }).add({
-            translateY: '0px',
-            easing: 'linear',
-            borderRadius: ['0%', '50%']
-        });
-    }
-}
-);
+    const a2 = anime
+      .timeline({
+        targets: ".btn",
+        duration: 100,
+        easing: "linear"
+      })
+      .add({
+        targets: ".chat",
+        opacity: 0
+      })
+      .add({
+        width: "70px",
+        height: "70px",
+        bottom: "20px"
+      })
+      .add({
+        translateY: "0px",
+        easing: "linear",
+        borderRadius: ["0%", "50%"]
+      });
+  }
+});
